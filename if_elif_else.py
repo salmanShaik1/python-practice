004e269c453c6274063e577965da1b03793c94c0
salmanShaik1 <shaiksalman984890@gmail.com>
2026-01-20 00:31:53 +0530

    if elif else learning

---
+++ if_elif_else.py
@@ -0,0 +1,13 @@
+age=18
+if(age>=18):
+    print("drive, vote")
+if(age>=16):
+    print("he is just passed 10th")
+
+if(age>=18):
+    print("he can drive and vote")
+elif(age>=16):
+    print("he is just passed 10th")
+else:
+    print("he is not ready for above")
+
