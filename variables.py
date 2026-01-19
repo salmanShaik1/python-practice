c118b79fb9d9c09df93117971a6ea3e5e81365c5
salmanShaik1 <shaiksalman984890@gmail.com>
2026-01-19 23:36:54 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,10 @@
+# A variable is a name given to a memory location in a program
+name="salman"
+age=24
+price=25.4
+print(type(name)) # these are datatypes
+print(type(age))
+print(type(price))
+
+#types of data types:
+#integers, string,float, boolean, none
