d34681eddceab7d19439cf93c1b3d11da6c9ed0f
salmanShaik1 <shaiksalman984890@gmail.com>
2026-01-20 00:28:48 +0530

    some more code

--- string functions.py
+++ string functions.py
@@ -2,6 +2,6 @@ str="i am a coder"
 print(str.endswith("er"))
 print(str.capitalize())
 print(str.replace(' ','@'))
-str="i am a coder am"
+str="i am a coder amam"
 print(str.find('am'))# returns first occurence index
 print(str.count('am'))
